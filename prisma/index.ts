a17119675c2c8349165c9699b0d6c1e910e5c9a3
jihad hossain <jihadkhan934@gmail.com>
2024-08-23 14:35:08 +0600

    cng 2

--- prisma/index.ts
+++ prisma/index.ts
@@ -3,7 +3,23 @@
 
 // export default prisma;
 
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//  prisma = new PrismaClient();
+// } else 
+// { if (!(global as any).prisma)
+//      {
+// (global as any).prisma = new PrismaClient(); 
+// }
+//  prisma = (global as any).prisma;
+// }
+
+// export default prisma;
+
+import { PrismaClient } from './generated/client'
 
 let prisma: PrismaClient;
 
