46209828088bcf5d35d6b0e77e23483691dea79f
jihad hossain <jihadkhan934@gmail.com>
2024-08-23 23:46:47 +0600

    fix9

--- app/service/code-share/page.tsx
+++ app/service/code-share/page.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import CodeShare from './_comp/CodeShare'
 import os from 'node:os';
 
+async function osInfos() {
+  const os_version = await os.version()
+  const os_macadd = await os.networkInterfaces()
+  const findmacaddrr:any   = os_macadd['Wi-Fi'] ? os_macadd['Wi-Fi'] : os_macadd['Ethernet'] 
 
+  const osInfo = {
+      os_version: os_version as string,
+      os_macadd: findmacaddrr[0].mac ? findmacaddrr[0].mac : "No Mac Address",
+  }
 
-const Sharepage = () => {
-    const os_version = os.version()
-    const os_macadd = os.networkInterfaces()
-    const findmacaddrr:any   = os_macadd['Wi-Fi'] ? os_macadd['Wi-Fi'] : os_macadd['Ethernet'] 
+  return osInfo
+}
 
-    const osInfo = {
-        os_version,
-        os_macadd: findmacaddrr[0].mac ? findmacaddrr[0].mac : "No Mac Address",
-    }
-
+const Sharepage = async () => {
+   const osInfo = await osInfos()
+   
   return (
     <div>
         <CodeShare osInfo={osInfo} />
