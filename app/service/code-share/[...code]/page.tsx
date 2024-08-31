24fb1cb55beabea70d684126a380a8081701db33
jihad hossain <jihadkhan934@gmail.com>
2024-08-31 11:55:30 +0600

    fix 41

--- app/service/code-share/[...code]/page.tsx
+++ app/service/code-share/[...code]/page.tsx
@@ -6,24 +6,21 @@ import { TextView } from './_comp/TextView';
 
 const CodeSharePage = async ({ params }: { params: { code: string[] } }) => {
   const isAuth:any = await serverAuth();
-  console.log("🚀 ~ CodeSharePage ~ isAuth:", isAuth)
   const findAuthLink = await fetch(`${process.env.PUBLIC_NEXT_URL}/api/v1/code-share?code=${params?.code[0]}&type=permanent`);
   const auth_link_json = await findAuthLink.json();
-
-  console.log("🚀 ~ CodeSharePage ~ auth_link_json:", auth_link_json?.result?.authorId)
 
   const isAuthLink = auth_link_json?.result?.authorId !== isAuth?.userId
   return (
     <div>
         {
-          !isAuth && <TextEditor params={params} />
+          !isAuth && !isAuthLink && <TextEditor params={params} />
         }
         {
           isAuth && auth_link_json?.result?.authorId == isAuth?.userId ? <AuthTextEditor params={params} /> : null
-        }
+        } 
 
         {
-          isAuthLink  && <TextView auth_link_json={auth_link_json} />
+          isAuthLink  && <TextView auth_link_json={auth_link_json?.result} />
         }
     </div>
   )
